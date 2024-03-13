b0ec52925750b43ce2f937d5f081f2f9aafc113a
unknown <ingridiaim@gmail.com>
2024-03-13 12:56:11 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/pinball-board/',
+  base: '/192.168.1.59/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
